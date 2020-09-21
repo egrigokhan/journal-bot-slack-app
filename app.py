3cb369fa59d81a4c3a697ccbf07e068e8536e238
Gokhan Egri <egrigokhan@gmail.com>
2020-09-22 00:36:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,32 @@ def slash():
 
 if __name__ == '__main__':
     app.run()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+class Paper:
+    def __init__(self, title, URL, description, pros = [], cons = [], voters = []):
+        self.title = title
+        self.URL = URL
+        self.description = description
+        self.pros = pros
+        self.cons = cons
+        self.voters = voters
+
+    def add_pro(self, pro):
+        self.pros.append(pro)
+        
+    def add_con(self, con):
+        self.cons.append(con)
+        
+    def get_short_message(self):
+        return ""
+    
+    def get_long_message(self):
