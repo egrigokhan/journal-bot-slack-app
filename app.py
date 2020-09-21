fe5062fa31e1d123566b9f88534e856ba4160128
Gokhan Egri <egrigokhan@gmail.com>
2020-09-22 00:42:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,32 @@ if __name__ == '__main__':
     
     
     
+class System:
+    def __init__(self, papers):
+        self.papers = papers
+        
+    def add_paper(self, paper):
+        self.papers.append(paper)
+        return "Paper succesfully added!"
+        
+    def get_current_message(self):
+        return ""
     
+    def get_detail_for_paper(self, index):
+        if(index < self.papers.count):
+            return self.papers[index].get_long_message()
+        
+    def add_pro_for_paper(self, index, pro):
+        if(index < self.papers.count):
+            return self.papers[index].add_pro(pro)
+        
+    def add_con_for_paper(self, index, con):
+        if(index < self.papers.count):
+            return self.papers[index].add_con(con)
+        
+    def add_or_remove_vote_from_paper(self, index, voter):
+        if(index < self.papers.count):
+            return self.papers[index].add_or_remove_vote(voter)
     
 class Paper:
     def __init__(self, title, URL, description, pros = [], cons = [], voters = []):
@@ -36,6 +61,12 @@ class Paper:
         self.pros = pros
         self.cons = cons
         self.voters = voters
+        
+    def add_or_remove_vote(self, voter):
+        if(voters.contains(voter)):
+            voters.remove(voter)
+        else:
+            voters.append(voter)
 
     def add_pro(self, pro):
         self.pros.append(pro)
